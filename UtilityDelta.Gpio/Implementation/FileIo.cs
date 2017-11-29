675013d668f6cf2b898f5a1472ce41cec72097d9
Tyson Brown <tysonbrown@live.com.au>
2017-11-29 21:11:27 +1000

    Added fileIo implementation

---
+++ UtilityDelta.Gpio/Implementation/FileIo.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using UtilityDelta.Gpio.Interfaces;
+
+namespace UtilityDelta.Gpio.Implementation
+{
+    public class FileIo : IFileIo
+    {
+        public void WriteAllText(string path, string contents)
+        {
+            File.WriteAllText(path, contents);
+        }
+
+        public string ReadAllText(string path) => File.ReadAllText(path);
+    }
+}
